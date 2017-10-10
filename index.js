f32716b6abdb6c7b4ba49070f9bc46955c9bf82c
alex-rokabilis <alekos_l2@hotmail.com>
2017-10-10 21:57:49 +0300

    add dialogflow

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 const app = require('http').createServer(handler),
- io = require('socket.io')(app),
- fs = require('fs'),
- ip = require("ip"),
- sensor = require('./sensor');
+    io = require('socket.io')(app),
+    fs = require('fs'),
+    ip = require("ip"),
+    sensor = require('./sensor'),
+    DialogflowApp = require('actions-on-google').DialogflowApp;
 
 app.listen(3001, () => console.log(`listening on http://localhost:3001`))
 
 
 function handler(req, res) {
-    
-    console.log(req);
-    fs.readFile(__dirname + '/index.html',
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
 
-            res.writeHead(200);
-            res.end(data);
+    if (req.url == '/') {
+        fs.readFile(__dirname + '/index.html',
+            function (err, data) {
+                if (err) {
+                    res.writeHead(500);
+                    return res.end('Error loading index.html');
+                }
+
+                res.writeHead(200);
+                res.end(data);
+            });
+    } else {
+
+        const app = new DialogflowApp({
+            request: req,
+            response: res
         });
+
+        const actionMap = new Map();
+        actionMap.set('tell.what_need_to_know', function numberIntent(app) {
+            const number = app.getArgument(NUMBER_ARGUMENT);
+            app.tell('You said ' + number);
+        });
+        app.handleRequest(actionMap);
+    }
+
 }
 
 
 setInterval(() => {
     sensor.read()
-        .then(({temperature,humidity}) =>{
+        .then(({
+            temperature,
+            humidity
+        }) => {
             io.of('/').emit('temperature', temperature);
             io.of('/').emit('humidity', humidity);
         })
-},1000)
+}, 1000)
