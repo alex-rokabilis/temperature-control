f6630742fd4d2656b9c5480492ea2a21bdcbe1d5
alex-rokabilis <alekos_l2@hotmail.com>
2017-10-10 22:04:32 +0300

    change to method instead or url

--- index.js
+++ index.js
@@ -10,35 +10,31 @@ app.listen(3001, () => console.log(`listening on http://localhost:3001`))
 
 function handler(req, res) {
 
-    if (req.url == '/') {
-        fs.readFile(__dirname + '/index.html',
-            function (err, data) {
-                if (err) {
-                    res.writeHead(500);
-                    return res.end('Error loading index.html');
-                }
+    if (req.method == 'POST') {
+        const app = new DialogflowApp({
+            request: req,
+            response: res
+        });
 
-                res.writeHead(200);
-                res.end(data);
-            });
+        const actionMap = new Map();
+        actionMap.set('tell.what_need_to_know', function numberIntent(app) {
+            const number = app.getArgument("need_to_know");
+            app.tell('You said ' + number);
+        });
+        app.handleRequest(actionMap);
     } else {
 
-        try{
-            const app = new DialogflowApp({
-                request: req,
-                response: res
-            });
-    
-            const actionMap = new Map();
-            actionMap.set('tell.what_need_to_know', function numberIntent(app) {
-                const number = app.getArgument("need_to_know");
-                app.tell('You said ' + number);
-            });
-            app.handleRequest(actionMap);
-        }catch(err){
-            
-        }
+        fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
 
+            res.writeHead(200);
+            res.end(data);
+        });
+        
     }
 
 }
